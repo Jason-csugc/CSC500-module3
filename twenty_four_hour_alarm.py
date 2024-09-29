89948c3dec29dd69c210eda6ed20c6382d41995f
tinidian <tinidian@gmail.com>
2024-09-29 08:01:33 -0600

    Finished part 2

--- twenty_four_hour_alarm.py
+++ twenty_four_hour_alarm.py
@@ -1,3 +1,10 @@
+import math
 
-current_hour = input('What is the current time in hours?')
-alarm_hours = input('How many hours do you wnat to wait for the alarm?')
+current_hour = int(input('What is the current time in hours (0-23)?'))
+alarm_hours = int(input('How many hours do you wnat to wait for the alarm?'))
+
+hours_to_add = alarm_hours % 24
+
+new_time = int((current_hour + hours_to_add) % 23)
+
+print(f'Your alarm will ring at {new_time:02}:00')
